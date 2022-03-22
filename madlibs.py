fd8d676a03ccc4f87e468a4fc82865a8fee97194
Richard Nkulu <richardsobelnkulu@gmail.com>
2022-03-22 08:59:08 +0200

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+    # string concatenation
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2= input("verb: ")
+famous_person = input("Famous person:")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time beacause \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+    
+print(madlib)
