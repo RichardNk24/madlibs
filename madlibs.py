baa2e10f8cb851cb8fd09b1a2338b684d8938cff
Richard Nkulu <64165035+RichardNk24@users.noreply.github.com>
2022-03-30 23:08:10 +0200

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -6,6 +6,6 @@ verb2= input("verb: ")
 famous_person = input("Famous person:")
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time beacause \
-    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person                                                             }"
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person                                                            }"
     
 print(madlib)
